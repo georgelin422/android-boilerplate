ddd26c317cd16b7ab54541911d3ab905fe1c4f8d
George Lin <chenhao_lin@htc.com>
2016-01-12 18:04:22 +0800

    Add Observable of Presenter Event - no need to keep reference of subscription anymore

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BasePresenter.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BasePresenter.java
@@ -1,5 +1,9 @@
 package uk.co.ribot.androidboilerplate.ui.base;
 
+import rx.Observable;
+import rx.functions.Func1;
+import rx.subjects.BehaviorSubject;
+
 /**
  * Base class that implements the Presenter interface and provides a base implementation for
  * attachView() and detachView(). It also handles keeping a reference to the mvpView that
@@ -7,15 +11,19 @@ package uk.co.ribot.androidboilerplate.ui.base;
  */
 public class BasePresenter<T extends MvpView> implements Presenter<T> {
 
+    private final BehaviorSubject<PresenterEvent> subject = BehaviorSubject.create();
+
     private T mMvpView;
 
     @Override
     public void attachView(T mvpView) {
         mMvpView = mvpView;
+        subject.onNext(PresenterEvent.ATTACH_VIEW);
     }
 
     @Override
     public void detachView() {
+        subject.onNext(PresenterEvent.DETACH_VIEW);
         mMvpView = null;
     }
 
@@ -28,14 +36,34 @@ public class BasePresenter<T extends MvpView> implements Presenter<T> {
     }
 
     public void checkViewAttached() {
-        if (!isViewAttached()) throw new MvpViewNotAttachedException();
+        if (!isViewAttached())
+            throw new MvpViewNotAttachedException();
     }
 
     public static class MvpViewNotAttachedException extends RuntimeException {
         public MvpViewNotAttachedException() {
             super("Please call Presenter.attachView(MvpView) before" +
-                    " requesting data to the Presenter");
+                " requesting data to the Presenter");
         }
     }
+
+    /**
+     * Returns an {@link Observable} of {@link PresenterEvent}.
+     *
+     * @param presenterEvent
+     * @return
+     */
+    public Observable<PresenterEvent> presenterEvent(final PresenterEvent presenterEvent) {
+        return subject.takeFirst(new Func1<PresenterEvent, Boolean>() {
+            @Override public Boolean call(PresenterEvent event) {
+                return event == presenterEvent;
+            }
+        });
+    }
+
+    protected enum PresenterEvent {
+        ATTACH_VIEW, DETACH_VIEW
+    }
+
 }
 
--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/MainPresenter.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/main/MainPresenter.java
@@ -31,34 +31,36 @@ public class MainPresenter extends BasePresenter<MainMvpView> {
     @Override
     public void detachView() {
         super.detachView();
-        if (mSubscription != null) mSubscription.unsubscribe();
+        if (mSubscription != null)
+            mSubscription.unsubscribe();
     }
 
     public void loadRibots() {
         checkViewAttached();
         mSubscription = mDataManager.getRibots()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Subscriber<List<Ribot>>() {
-                    @Override
-                    public void onCompleted() {
-                    }
+            .takeUntil(presenterEvent(PresenterEvent.DETACH_VIEW))
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(new Subscriber<List<Ribot>>() {
+                @Override
+                public void onCompleted() {
+                }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.e(e, "There was an error loading the ribots.");
-                        getMvpView().showError();
-                    }
+                @Override
+                public void onError(Throwable e) {
+                    Timber.e(e, "There was an error loading the ribots.");
+                    getMvpView().showError();
+                }
 
-                    @Override
-                    public void onNext(List<Ribot> ribots) {
-                        if (ribots.isEmpty()) {
-                            getMvpView().showRibotsEmpty();
-                        } else {
-                            getMvpView().showRibots(ribots);
-                        }
+                @Override
+                public void onNext(List<Ribot> ribots) {
+                    if (ribots.isEmpty()) {
+                        getMvpView().showRibotsEmpty();
+                    } else {
+                        getMvpView().showRibots(ribots);
                     }
-                });
+                }
+            });
     }
 
 }
